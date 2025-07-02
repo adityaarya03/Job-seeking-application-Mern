51d6723bc08ce168c1e966ee8c3723436c39174d
adityaarya03 <adityadrifter@icloud.com>
2025-07-03 03:47:57 +0530

    working-login

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:process.env.CLIENT_URL,
+    origin:process.env.CLIENT_URL || "http://localhost:5173",
     credentials:true
 }
 
